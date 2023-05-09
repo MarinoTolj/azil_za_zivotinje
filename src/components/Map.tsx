0abb0c2d2eff8c0681d250f39bfe14378db96128
MarinoTolj <marino.tolj7@gmail.com>
2023-05-09 13:17:17 +0200

    icon images for map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-icon-2x.png";
 
 /**
  * Pics
