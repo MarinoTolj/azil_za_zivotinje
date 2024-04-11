7d5f942d937b39faab68989bf0911a6dd205b210
MarinoTolj <marino.tolj7@gmail.com>
2024-04-11 18:41:37 +0200

    Init for backend

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+app.get("/test", (req: Request, res: Response) => {
+  res.send("Test");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
